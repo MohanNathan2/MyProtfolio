f463f1071059edc0b5cb833dbae9464c9b79e937
MohanNathan1 <mohan@technorishiconsulting.com>
2023-09-26 09:26:22 +0530

    config file

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     assetsDir: "assets", // Specify your assets directory
+    rollupOptions: {
+      external: ['emailjs-com'], // Add any other external modules as needed
+    },
   },
 });
